4a74aa56d3fd39547c263cbdf57175ba5b401537
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-01-04 05:36:08 -0500

    pilot and copilot display

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+  let pilotStatus = document.getElementById("pilotStatus");
+  let copilotStatus = document.getElementById("copilotStatus");
+  let fuelStatus = document.getElementById("fuelStatus");
+  let cargoStatus = document.getElementById("cargoStatus");
+  let launchStatus = document.getElementById("launchStatus");
+  let faultyItemsDiv = document.getElementById("faultyItems");
+
   let form = document.querySelector("form");
   form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
     let pilotName = document.querySelector("input[name=pilotName]");
     let copilotName = document.querySelector("input[name=copilotName]");
     let fuelLevel = document.querySelector("input[name=fuelLevel]");
     let cargoMass = document.querySelector("input[name=cargoMass]");
     if (
       pilotName.value === "" ||
-      coPilotName.value === "" ||
+      copilotName.value === "" ||
       cargoMass.value === "" ||
       fuelLevel.value === ""
     ) {
-      alert("All fields are required!");
-      // stop the form submission
-      event.preventDefault();
+      alert("All fields are required.");
+    } else {
+      faultyItemsDiv.style.visibility = "visible";
+      pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch.`;
+      copilotStatus.innerHTML = `Copilot ${copilotName.value} is ready for launch.`;
     }
   });
 });
